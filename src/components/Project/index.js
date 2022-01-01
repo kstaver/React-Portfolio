79b35a23c7e199aaa826eb3d54376dfc14506f30
Kendra Staver <staverkendra@gmail.com>
2021-12-31 23:58:28 -0800

    finished portfolio index template

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,1 +1,24 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function ProjectCards(props){
+    return(
+        <div className="card">
+            <div className="img-container">
+                <img alt="{props.name}" src={props.image}/>
+            </div>
+            <div className="content">
+                <p className="card-title">{props.name}</p>
+                <p class="proj-icons-container">
+                    <a href={props.github}><img class="project-icon" src="" alt=""/></a>
+                    <a href={props.deploy}><img class="project-icon" src="" alt=""/></a>
+                </p>
+                <p class="topics">
+                    ({props.topics})
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default ProjectCards;
