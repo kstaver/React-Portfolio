91ed73469dd0e50fb3ef7a87dabe19ca533db636
Kendra Staver <staverkendra@gmail.com>
2021-12-31 20:30:38 -0800

    started css and created skeleton js for About

---
+++ src/components/About/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function About(){
+    return(
+        <section className="container">
+            <hr></hr>
+            <div className="row">
+                <img class="mb-5" id="avatar" src="https://avatars.githubusercontent.com/32945233" alt="Kendra Staver"></img>
+                <div>
+                    <p>
+
+                    </p>
+                    <p>
+
+                    </p>
+                    <p>
+
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default About;
