0e23bf22097cf0dcbac5751d5766dba23638e331
Kendra Staver <staverkendra@gmail.com>
2022-01-01 00:20:39 -0800

    happy new year. finished resume index

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,1 +1,37 @@
 import React from 'react';
+
+function Resume(){
+    return(
+        <section class="container">
+            <h2 className="top-title">Resume</h2>
+            <hr></hr>
+            <div>
+                <div class="mt-5">
+                    <h2 className="top-title">Kendra Staver</h2>
+                    <ul>
+                        <li>
+                            Full-Stack Web Developer, Mathematician
+                        </li>
+                    </ul>
+
+                    <p class="mt-5">
+                        <a href="https://www.linkedin.com/in/kendra-staver/">
+                            <img
+                                src=""
+                                alt="linkedIn"
+                            />
+                        </a>
+                        <a
+                            href=""
+                            class="link"
+                        >
+                            Download Resume
+                        </a>
+                    </p>
+                </div>            
+            </div>
+        </section>
+    );
+}
+
+export default Resume;
