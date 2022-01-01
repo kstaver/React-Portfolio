b9a9fc168afb8b1da51446acd55bcd3d808133e4
Kendra Staver <staverkendra@gmail.com>
2021-12-31 23:07:10 -0800

    finished footer template

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,1 +1,13 @@
 import React from 'react';
+
+function Footer(){
+    return(
+        <footer className="footer bg-dark fixed-bottom">
+            <a href=""><img src="" alt="Github" className="icon"/></a>
+            <a href=""><img src="" alt="LinkedIn" className="icon"/></a>
+            <a href=""><img src="" alt="AngelList" className="icon"/></a>
+        </footer>
+    )
+}
+
+export default Footer;
