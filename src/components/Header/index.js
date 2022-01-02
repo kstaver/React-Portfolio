49751842d08a0335d2e8869cdabcbb8354c1ab50
kstaver <staverkendra@gmail.com>
2022-01-02 02:54:43 -0800

    fixed sending component to element error

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -15,13 +15,13 @@ class Header extends React.Component {
             </header>
 
         <div className="content">
-            <Routes>
-                <Route exact path="/" element={<Navigate to="/about" replace/>} />
-                <Route path="/about" element={About} />
-                <Route path="/portfolio" element={Portfolio} />
-                <Route path="/contact" element={Contact}/>
-                <Route path="/resume" element={Resume}/>
-            </Routes>
+        <Routes>
+            <Route exact path="/" element={<Navigate to="/about" replace/>} />
+            <Route path="/about" element={<About />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/resume" element={<Resume />} />
+        </Routes>
         </div>
     </div>
     );
