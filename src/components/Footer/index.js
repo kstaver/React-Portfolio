11b3f24b448690b86024ddcc2db2f0e5b9b9549e
kstaver <staverkendra@gmail.com>
2022-01-02 13:51:38 -0800

    added footer links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,8 +3,8 @@ import React from 'react';
 function Footer(){
     return(
         <footer className="footer bg-dark fixed-bottom">
-            <a href=""><img src="" alt="Github" className="icon"/></a>
-            <a href=""><img src="" alt="LinkedIn" className="icon"/></a>
+            <a href="https://github.com/kstaver"><img src="" alt="Github" className="icon"/></a>
+            <a href="https://www.linkedin.com/in/kendra-staver/"><img src="" alt="LinkedIn" className="icon"/></a>
         </footer>
     )
 }
